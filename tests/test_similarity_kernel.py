a131960f2a18d661a0048323401c8be1e13d2145
William C Grisaitis <william@grisait.is>
2020-01-12 23:34:45 -0500

    Add failing test - make kernel function work

--- tests/test_similarity_kernel.py
+++ tests/test_similarity_kernel.py
@@ -1,9 +1,14 @@
 import pytest
+import jax.numpy as np
+
+from network_fusion.similarity_kernel import SimilarityKernel
 
 
 def test_similarity_kernel():
-  kernel = SimilarityKernel(mu=1)
+  kernel = SimilarityKernel(mu=1.0)
   x = np.array([0])
   assert kernel(x, x) == 1
   x = np.array([3])
   assert kernel(x, x) == 1
+  x, y = np.array([0]), np.array([5])
+  assert kernel(x, y) == np.exp()
