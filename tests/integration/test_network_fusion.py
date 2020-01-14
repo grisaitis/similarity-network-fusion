d8bf2db7b503b7eec1aee5873761eba609a6dfe2
William C Grisaitis <william@grisait.is>
2020-01-14 09:58:55 -0500

    Implement epsilon and local similarity; extract computations into functions

--- tests/integration/test_network_fusion.py
+++ tests/integration/test_network_fusion.py
@@ -3,7 +3,8 @@ import heapq
 import numpy as np
 import scipy.spatial as spatial
 
-def calculate_distances(x: np.ndarray[np.float]):
+
+def calculate_distances(x: np.ndarray):
   """Calculates euclidean distances between each row of x
   
   Arguments:
@@ -19,46 +20,64 @@ def calculate_distances(x: np.ndarray[np.float]):
       distance[i, j] = distance[j, i] = spatial.distance.euclidean(x_i, x_j)
   return distance
 
-def test_makes_fused_network():
-  rs = np.random.RandomState(0)
-  n, p = 20, 10
-  x = random_state.uniform(size=n * p).reshape((n, p))
-  mu = 0.5
-  k_neighbors = 5
 
-  distance = calculate_distances(x)
-
-  neighbors = dict()
-  neighbor_distances = np.array((n, k), )
+def calculate_neighborhoods(distance, k_neighbors):
+  n = distance.shape[0]
+  neighbor_indices = np.empty((n, k_neighbors), dtype=np.uint32)
+  neighbor_distances = np.empty((n, k_neighbors), dtype=distance.dtype)
+  print(distance)
   for i, distance_i in enumerate(distance):
-    distances_from_i_excluding_i = [(distance_i_j, j) for j, distance_i_j in distance_i if i != j]
+    print(type(distance_i))
+    print(distance_i)
+    print("cookie dough")
+    distances_from_i_excluding_i = \
+      [(distance_i_j, j) for j, distance_i_j in enumerate(distance_i) if i != j]
     distances_and_neighbors = heapq.nsmallest(k_neighbors, distances_from_i_excluding_i)
-    neighbor_distances[i], neighbors[i] = zip(*distances_and_neighbors)
+    neighbor_distances[i], neighbor_indices[i] = zip(*distances_and_neighbors)
+  return neighbor_indices, neighbor_distances
 
+
+def calculate_epsilon(distance, neighbor_distances):
+  mean_neighbor_distance = np.mean(neighbor_distances, axis=1)
   epsilon = np.zeros_like(distance)
   for i, distance_i in enumerate(distance):
-    epsilon[i, i] = 2 * mean_distance_to_nearest[i] / 3
     for j, distance_i_j in enumerate(distance_i[i + 1:]):
-      epsilon[i, j] = epsilon[j, i] = (mean_distance_to_nearest[i] \
-        + mean_distance_to_nearest[j] + distance_i_j) / 3
-  weights = np.exp(-distance * distance / mu / epsilon)
+      epsilon[i, j] = epsilon[j, i] = (mean_neighbor_distance[i] + mean_neighbor_distance[j] + distance_i_j) / 3
+  return epsilon
 
-  normalized_weights = compute_normalized_weights(weights)
 
-  sparse_similarity = compute_sparse_similarity(weights, neighbors)
+def calculate_normalized_weights(weights):
+  raise NotImplementedError
 
+
+def calculate_local_similarities(weights, neighbor_indices):
+  n = weights.shape[0]
+  neighbor_weight_sums = np.zeros(shape=(n,), dtype=weights.dtype)
+  # to do: calculate and store neighbor_weight_sums
   inverse_neighborhood_weights = np.zeros_like(weights)
-  for i, distance_i in enumerate(distance):
-    neighbor_tuples = \
-      [(distance_i_j, j) for j, distance_i_j in distance_i if i != j]
-    # make neighborhood with nsmallest
-    k_smallest_tuples = heapq.nsmallest(k_neighbors, neighbor_tuples)
-    # add up weights[k] for k in neighborhood
-  '''
-  '''
+  for i, weights_i in enumerate(weights):
+    # add up weights[k_neighbors] for k_neighbors in neighborhood
+    for neighbor_index in neighbor_indices[i]:
+      inverse_neighborhood_weights[i, neighbor_index] = 1.0 / neighbor_weight_sums[i]
   similarity = weights * inverse_neighborhood_weights
-  # done with similarity
-  return distance, weights, similarity
-  # similarity_net_1 = SimilarityNetwork(X1)
-  # fuser = NetworkFuser()
-  # fused_network = fuser.fuse(networks)
+
+
+def assert_is_symmetrix(a, rtol=1e-05, atol=1e-08):
+  assert np.allclose(a, a.T, rtol=rtol, atol=atol)
+  return
+
+
+def test_makes_fused_network():
+  random_state = np.random.RandomState(0)
+  n, p = 5, 4
+  x = random_state.uniform(size=n * p).reshape((n, p))
+  mu = 0.5
+  k_neighbors = 3
+  distance = calculate_distances(x)
+  assert distance.shape == (n, n)
+  assert_is_symmetrix(distance)
+  neighbor_indices, neighbor_distances = calculate_neighborhoods(distance, k_neighbors)
+  epsilon = calculate_epsilon(distance, neighbor_distances)
+  weights = np.exp(-distance * distance / mu / epsilon)
+  normalized_weights = calculate_normalized_weights(weights)
+  local_similarities = calculate_local_similarities(weights, neighbor_indices)
