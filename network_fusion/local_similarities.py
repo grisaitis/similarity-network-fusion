c01b0b53221076963698738442229bea06a5ed1f
William C Grisaitis <wgrisaitis@gmail.com>
2020-01-15 17:36:18 -0500

    Fix imports, pep8

--- network_fusion/local_similarities.py
+++ network_fusion/local_similarities.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def calculate_local_similarities(weights, neighbor_indices):
   n = weights.shape[0]
   neighbor_weight_sums = np.zeros(shape=(n,), dtype=weights.dtype)
@@ -6,5 +9,6 @@ def calculate_local_similarities(weights, neighbor_indices):
   for i, weights_i in enumerate(weights):
     # add up weights[k_neighbors] for k_neighbors in neighborhood
     for neighbor_index in neighbor_indices[i]:
-      inverse_neighborhood_weights[i, neighbor_index] = 1.0 / neighbor_weight_sums[i]
-  similarity = weights * inverse_neighborhood_weights
+      inverse_neighborhood_weights[i, neighbor_index] = \
+        1.0 / neighbor_weight_sums[i]
+  return weights * inverse_neighborhood_weights
--- network_fusion/neighborhoods.py
+++ network_fusion/neighborhoods.py
@@ -8,8 +8,9 @@ def calculate_neighborhoods(distance, k_neighbors):
   neighbor_indices = np.empty((n, k_neighbors), dtype=np.uint32)
   neighbor_distances = np.empty((n, k_neighbors), dtype=distance.dtype)
   for i, distance_i in enumerate(distance):
-    distances_from_i_excluding_i = \
-      [(distance_i_j, j) for j, distance_i_j in enumerate(distance_i) if i != j]
-    distances_and_neighbors = heapq.nsmallest(k_neighbors, distances_from_i_excluding_i)
+    distances_from_i_excluding_i = [
+      (distance_i_j, j) for j, distance_i_j in enumerate(distance_i) if i != j]
+    distances_and_neighbors = heapq.nsmallest(
+      k_neighbors, distances_from_i_excluding_i)
     neighbor_distances[i], neighbor_indices[i] = zip(*distances_and_neighbors)
   return neighbor_indices, neighbor_distances
