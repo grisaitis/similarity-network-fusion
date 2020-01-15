43814e084e045a76eec4fc04cb65b5f0d18cbbc6
William C Grisaitis <wgrisaitis@gmail.com>
2020-01-15 18:28:12 -0500

    Add module & test for similarity network

--- tests/integration/test_network_fusion.py
+++ tests/integration/test_network_fusion.py
@@ -1,40 +1,5 @@
 import pytest
 
-import numpy as np
-
-from network_fusion.distances import calculate_distances
-from network_fusion.local_similarities import calculate_local_similarities
-from network_fusion.neighborhoods import calculate_neighborhoods
-from network_fusion.epsilon import calculate_epsilon
-from network_fusion.normalized_weights import calculate_normalized_weights
-from network_fusion.weights import calculate_weights
-
-
-def test_local_similarities():
-  random_state = np.random.RandomState(0)
-  n, p = 5, 4
-  x = random_state.uniform(size=n * p).reshape((n, p))
-  mu = 0.5
-  k_neighbors = 3
-  distances = calculate_distances(x)
-  neighbor_indices, neighbor_distances = \
-    calculate_neighborhoods(distances, k_neighbors)
-  epsilon = calculate_epsilon(distances, neighbor_distances)
-  weights = calculate_weights(distances, epsilon, mu)
-  diagonals_equal_1 = all(weights[i, i] == 1 for i in range(weights.shape[0]))
-  assert diagonals_equal_1
-  normalized_weights = calculate_normalized_weights(weights)
-  assert np.sum(normalized_weights, axis=0) == 1
-  assert np.sum(normalized_weights, axis=1) == 1
-  # to do: assert that diagonal values are 0.5
-  local_similarities = calculate_local_similarities(weights, neighbor_indices)
-  rows_are_sparse = np.sum(local_similarities != 0, axis=0) == k_neighbors
-  assert rows_are_sparse
-  columns_are_sparse = np.sum(local_similarities != 0, axis=0) == k_neighbors
-  assert columns_are_sparse
-  # to do: assert that local similarities are not symmetric
-  return normalized_weights, local_similarities
-
 
 @pytest.mark.skip("not implemented")
 def test_fusion_step():
