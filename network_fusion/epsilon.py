f18a923b124998efa418dbfcf3a455fd6c139d7b
William C Grisaitis <wgrisaitis@gmail.com>
2020-01-15 18:13:25 -0500

    Make epsilon computation more efficient

--- network_fusion/epsilon.py
+++ network_fusion/epsilon.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def calculate_epsilon(distance, neighbor_distances):
+def calculate_epsilon(distances, neighbor_distances):
   mean_neighbor_distance = np.mean(neighbor_distances, axis=1)
-  epsilon = np.ones_like(distance)
-  for i, distance_i in enumerate(distance):
-    for j, distance_i_j in enumerate(distance_i[i + 1:], start=i + 1):
-      epsilon[i, j] = epsilon[j, i] = (mean_neighbor_distance[i] + mean_neighbor_distance[j] + distance_i_j) / 3
+  assert mean_neighbor_distance.shape == (distances.shape[0],)
+  mean_i_plus_mean_j = \
+    mean_neighbor_distance[None, :] + mean_neighbor_distance[:, None]
+  epsilon = (mean_i_plus_mean_j + distances) / 3
   return epsilon
--- tests/unit/test_epsilon.py
+++ tests/unit/test_epsilon.py
@@ -4,7 +4,6 @@ import pytest
 from network_fusion.epsilon import calculate_epsilon
 
 
-@pytest.mark.skip("will do this later")
 def test_calculates_the_right_value_n_2():
   distances = np.array([
     [0, 3],
@@ -13,25 +12,6 @@ def test_calculates_the_right_value_n_2():
   neighbor_distances = np.array([[3], [3]])
   epsilon = calculate_epsilon(distances, neighbor_distances)
   assert np.allclose(epsilon, np.array([
-    [1, ]
-  ]))
-
-
-def test_diagonals_are_one():
-  distances = np.array([
-    [0, 0.1, 1, 3],
-    [0.1, 0, 1, 3],
-    [1, 1, 0, 2],
-    [3, 3, 2, 0]
-  ])
-  neighbor_distances = np.array([[0.1], [0.1], [1], [2]])
-  epsilon = calculate_epsilon(distances, neighbor_distances)
-  for i, row_i in enumerate(epsilon):
-    assert row_i[i] == 1
-
-
-@pytest.mark.skip("will do this later")
-def test_is_square_symmetric():
-  epsilon = None
-  assert epsilon.shape[0] == epsilon.shape[1], "must be square"
-  assert np.allclose(epsilon, epsilon.T), "must be symmetric"
+    [2.0, 3.0],
+    [3.0, 2.0]
+  ])), epsilon
