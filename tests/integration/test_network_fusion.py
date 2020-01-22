a863fa1c8bae968258996c121650a041e7d8154d
William C Grisaitis <wgrisaitis@gmail.com>
2020-01-22 13:29:31 -0500

    WIP - network fusion test. Pending SimilarityNetwork, and iterative fusing

--- tests/integration/test_network_fusion.py
+++ tests/integration/test_network_fusion.py
@@ -1,4 +1,8 @@
+import numpy as np
 import pytest
+
+from network_fusion.similarity_network import SimilarityNetwork
+from tests.helpers.fake_data import make_fake_data
 
 
 @pytest.mark.skip("not implemented")
@@ -11,7 +15,8 @@ def test_fusion_step():
 
 @pytest.mark.skip("WIP")
 def test_similarity_network_fusion():
-    '''
+    rs = np.random.RandomState(0)
+    x_1, x_2 = make_fake_data(random_state=rs), make_fake_data(random_state=rs)
     networks = SimilarityNetwork(x_1), SimilarityNetwork(x_2)
     fuser = NetworkFuser()
     network_iterations = [networks]
@@ -20,5 +25,4 @@ def test_similarity_network_fusion():
         networks_t = network_iterations[-1]
         networks_t_plus_one = fuser.fuse(networks_t)
         network_iterations.append(networks_t_plus_one)
-    '''
     pass
